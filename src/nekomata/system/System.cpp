cc972ef00c4fdbb339fd3c416f8e0500f2f9852a
psi <ledyba@users.sourceforge.jp>
2012-02-29 21:47:59 +0900

    ログ追加

--- src/nekomata/system/System.cpp
+++ src/nekomata/system/System.cpp
@@ -5,11 +5,12 @@
  *      Author: psi
  */
 
-#include "../Nekomata.h"
+#include <tr1/memory>
 #include "System.h"
+#include "../logging/Logging.h"
+#include "../tree/Node.h"
 #include "../object/Object.h"
 #include "../object/Heap.h"
-#include <tr1/memory>
 
 namespace nekomata{
 namespace system {
@@ -40,8 +41,8 @@ shared_ptr<Shape> System::drawShape(double x, double y, double z, const std::str
 shared_ptr<Chat> System::drawText(const std::string& text, double x, double y, double z, double size, const std::string& pos, unsigned int color, bool bold, bool visible, const std::string& filter, double alpha, const std::string& mover)
 {
 	log.v(TAG, 0,
-			"drawText(%s)",
-			text.c_str()
+			"drawText(text:%s , x: %f, y: %f, z: %f, size: %f, pos: %s, color: %d, bold: %d, visible: %d, filter: %s, alpha: %f, mover: %s)",
+			text.c_str(), x, y, z, size, pos.c_str(), color, bold, visible, filter.c_str(), alpha, mover.c_str()
 			);
 	shared_ptr<Chat> _chat(new Chat(*this));
 	_chat->load(text, x, y, z, size, pos, color, bold, visible, filter, alpha, mover);
@@ -49,34 +50,57 @@ shared_ptr<Chat> System::drawText(const std::string& text, double x, double y, d
 }
 void System::commentTrigger(float const timer, const tree::Node* then)
 {
-
+	log.v(TAG, 0,
+			"commentTrigger(timer: %f, node: (%d, %d in %s))",
+			timer, then->location().getLineNo(), then->location().getColNo(), then->location().getFilename().c_str()
+			);
 }
 void System::timer(float const timer, const tree::Node* then)
 {
-
+	log.v(TAG, 0,
+			"timer(timer: %f, node: (%d, %d in %s))",
+			timer, then->location().getLineNo(), then->location().getColNo(), then->location().getFilename().c_str()
+			);
 }
 void System::jump(const std::string& id, const std::string& msg, double from, double length, bool _return, const std::string& returnmsg, bool newwindow)
 {
+	log.v(TAG, 0,
+			"jump(id:%s , msg:%s , from: %d, length: %d, _return: %d, returnmsg: %s,newwindow: %d)",
+			id.c_str(), msg.c_str(), from, length, _return, returnmsg.c_str(),newwindow
+			);
 
 }
 void System::jumpCancel()
 {
-
+	log.v(TAG, 0,"jumpCancel()");
 }
 void System::seek(double vpos, const std::string& msg)
 {
-
+	log.v(TAG, 0,
+			"seek(vpos: %f, msg: %s)",
+			vpos, msg.c_str()
+			);
 }
 void System::addMarker(const std::string& name, double vpos)
 {
-
+	log.v(TAG, 0,
+			"addMarker(name: %s, vpos: %f)",
+			name.c_str(), vpos
+			);
 }
 double System::getMarker(const std::string& name)
 {
-
+	log.v(TAG, 0,
+			"getMarker(name: %s)",
+			name.c_str()
+			);
 }
 shared_ptr<Sum> System::sum(double x, double y, double size, unsigned int color,bool visible, bool enabled, const std::string& pos, bool asc, const std::string& unit, bool buttononly, const std::vector<std::string>& words, bool partial)
 {
+	log.v(TAG, 0,
+			"sum(x: %f, y: %f, size: %f, color: %x, visible: %d, enabled: %d, pos: %s, asc: %d, unit: %s, buttononly: %d, words: %d word(s), partial: %d)",
+			x, y, size, color, visible, enabled, pos.c_str(), asc, unit.c_str(), buttononly, words.size(), partial
+			);
 	shared_ptr<Sum> _sum(new Sum(*this));
 	_sum->load(x, y, size, color, visible, enabled, pos, asc, unit, buttononly, words, partial);
 	return _sum;
@@ -84,59 +108,91 @@ shared_ptr<Sum> System::sum(double x, double y, double size, unsigned int color,
 shared_ptr<SumResult> System::showResult(double x, double y, unsigned int color,bool visible, const std::string& pos, const std::string& unit, bool asc, std::vector<shared_ptr<Sum> > sum)
 {
 	shared_ptr<SumResult> _sumResult(new SumResult(*this));
+	log.v(TAG, 0,
+			"showResult(x: %f, y: %f, color: %x, visible: %d, pos: %s, asc: %d, unit: %s, sum: %ditem(s))",
+			x, y, color, visible, pos.c_str(), asc, unit.c_str(), sum.size()
+			);
 	_sumResult->load(x, y, color, visible, pos, unit, asc, sum);
 	return _sumResult;
 }
 shared_ptr<Replace> System::replace(const std::string& src, const std::string& dst, bool enabled, const std::string& target, bool fill, bool partial, unsigned int color, const std::string& size, const std::string& pos)
 {
 	shared_ptr<Replace> _replace(new Replace(*this));
+	log.v(TAG, 0,
+			"replace(src: %s, dst: %s, enabled: %d, target: %s, fill: %d, partial: %d, color: %x, size: %s, pos: %s)",
+			src.c_str(), dst.c_str(), enabled, target.c_str(), fill, partial, color, size.c_str(), pos.c_str()
+			);
 	_replace->load(src, dst, enabled, target, fill, partial, color, size, pos);
 	return _replace;
 }
 double System::screenWidth()
 {
+	log.v(TAG, 0,"screenWidth()");
 
 }
 double System::screenHeight()
 {
+	log.v(TAG, 0,"screenHeight()");
 
 }
 shared_ptr<Button> System::addButton(const std::string& message, const std::string& mail, double vpos, const std::string& commes, const std::string& commail, bool comvisible, int limit, bool hidden)
 {
+	log.v(TAG, 0,
+			"addButton(message: %s, mail: %s, vpos: %f, commes: %s, commail: %s, comvisible: %d, limit: %d, hidden:%d)",
+			message.c_str(), mail.c_str(), vpos, commes.c_str(), commail.c_str(), comvisible, limit, hidden
+			);
 	shared_ptr<Button> _button(new Button(*this));
 	_button->load(message, mail, vpos, commes, commail, comvisible, limit, hidden);
 	return _button;
 }
 double System::playStartTime()
 {
+	log.v(TAG, 0,"playStartTime()");
 
 }
 void System::BGM(const std::string& id, double x, double y, double width, double height, bool visual, double volume)
 {
-
+	log.v(TAG, 0,
+			"BGM(id: %s, x: %f, y: %f, width: %f, height: %f, visual: %d, volume: %f)",
+			id.c_str(), x, y, width, height, visual, volume
+			);
 }
 void System::playBGM(int id)
 {
+	log.v(TAG, 0,"playBGM(id: %d)", id);
 
 }
 void System::stopBGM(int id)
 {
-
+	log.v(TAG, 0,"stopBGM(id: %d)", id);
 }
 void System::addAtPausePoint(double vpos, double wait)
 {
+	log.v(TAG, 0,
+			"addAtPausePoint(vpos: %f, wait: %f)",
+			vpos, wait
+			);
 
 }
 void System::addPostRoute(const std::string& match, const std::string& id, const std::string& button)
 {
+	log.v(TAG, 0,
+			"addPostRoute(match: %s, id: %s, button: %s)",
+			match.c_str(), id.c_str(), button.c_str()
+			);
 
 }
 void System::CM(const std::string& id, double time, bool pause, const std::string& link, double volume)
 {
+	log.v(TAG, 0,
+			"addPostRoute(id: %s, time: %f, pause: %d, link: %s, volume: %f)",
+			id.c_str(), time, pause, link.c_str(), volume
+	);
 
 }
 void System::playCM(int id)
 {
+	log.v(TAG, 0,"playCM(id: %d)", id);
 
 }
 
