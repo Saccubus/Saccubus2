b2dbc5078caaff8e01f8720b3a41db8438e89f2b
psi <ledyba@users.sourceforge.jp>
2012-03-30 15:13:13 +0900

    IEのキャッシュ追加

---
+++ src/saccubus/net/thread.py
@@ -0,0 +1,16 @@
+#! python3
+# -*- coding: utf-8 -*-
+'''
+Created on 2012/03/30
+
+@author: psi
+'''
+
+'''
+コメントをDLします。
+'''
+def downloadThread(jar, play_info, resDir):
+	pass
+
+def mergeThread(base, appended):
+	pass
