20d09833bbc2c38fef946e3d42bd210373c9e71e
psi <ledyba@users.sourceforge.jp>
2012-04-04 11:21:18 +0900

    接続はこんな感じ？

--- src/saccubus/Saccubus.cpp
+++ src/saccubus/Saccubus.cpp
@@ -5,6 +5,8 @@
  *      Author: psi
  */
 
+#include <getopt.h>
+#include <cstdlib>
 #include <libgen.h>
 #include "python/PyBridge.h"
 #include "util/StringUtil.h"
@@ -17,32 +19,126 @@ namespace saccubus {
 static const std::string RESOLVE_PREFIX("--resolve-");
 static const std::string TAG("Saccubus");
 
-Saccubus::Saccubus(logging::Logger& log, int argc, char** argv)
-:progPath(dirname(const_cast<char*>(argv[0])))
-,bridge(new python::PyBridge(log))
-{
-	if(argc < 2){
-		throw logging::Exception("Called with too few arguments: %d", argc);
-	}
-	nowVideoId = argv[1];
-	log.i(TAG, "Video ID: %s", nowVideoId.c_str());
+const struct option ARG_OPTIONS[] = {
+		{"trace", no_argument, 0, 1},
+		{"verbose", no_argument, 0, 2},
+		{"debug", no_argument, 0, 3},
+		{"info", no_argument, 0, 4},
+		{"warning", no_argument, 0, 5},
+		{"error", no_argument, 0, 6},
+		{"help", no_argument, 0, 7},
+		{"version", no_argument, 0, 8},
+		{0,0,0,0}
+};
 
-	for(int i=2;i<argc;++i){
-		std::string arg(argv[i]);
-		if(i+1<argc && util::startsWith(arg, RESOLVE_PREFIX)){
-			++i;
-			this->resolveOpts.push_back(std::pair<std::string, std::string>(arg.substr(RESOLVE_PREFIX.size()), argv[i]));
+void usage(std::ostream& logStream, int argc, char* argv[]){
+	static const std::string USAGE_TAB="    ";
+/*
+	cout << "Usage: " << basename(argv[0]) << " [switches] [--] [programfile]" << endl;
+	cout << USAGE_TAB << std::left << std::setw(15) << "--trace" << "set log level." << endl;
+	cout << USAGE_TAB << std::left << std::setw(15) << "--verbose" << "set log level." << endl;
+	cout << USAGE_TAB << std::left << std::setw(15) << "--debug"<<"set log level." << endl;
+	cout << USAGE_TAB << std::left << std::setw(15) << "--warning"<<"set log level." << endl;
+	cout << USAGE_TAB << std::left << std::setw(15) << "--error"<<"set log level." << endl;
+	cout << USAGE_TAB << std::left << std::setw(15) << "--dump"<<"output dump of AST, then exit." << endl;
+	cout << USAGE_TAB << std::left << std::setw(15) << "--version"<<"output the version, then exit." << endl;
+	cout << USAGE_TAB << std::left << std::setw(15) << "-h, --help"<<"output the help, then exit." << endl;
+*/
+	exit(0);
+}
+
+void version(std::ostream& logStream, int argc, char* argv[]){
+/*
+		cout << nekomata::PROGRAM_NAME << ": "<< nekomata::PROGRAM_VERSION <<" (build at " << __DATE__ << " " << __TIME__ << " )" << endl;
+*/
+	exit(0);
+}
+
+Saccubus::Saccubus(std::ostream& logStream, int argc, char** argv)
+:progPath(dirname(const_cast<char*>(argv[0])))
+,bridge(0)
+{
+	logging::Logger::Level level = logging::Logger::WARN_;
+	int index = 0;
+	while ( 1 )
+	{
+		/*
+		 * CLI、ffmpegの双方で同じオプションを使えるようにするために、
+		 * ここでオプションをパースする。
+		 */
+		int opt = getopt_long(argc, argv, "h", ARG_OPTIONS, &index);
+		if ( opt < 0 )
+		{
+			break;
+		}
+		switch (opt)
+		{
+		case 1:
+			level = logging::Logger::TRACE_;
+			break;
+		case 2:
+			level = logging::Logger::VERBOSE_;
+			break;
+		case 3:
+			level = logging::Logger::DEBUG_;
+			break;
+		case 4:
+			level = logging::Logger::INFO_;
+			break;
+		case 5:
+			level = logging::Logger::WARN_;
+			break;
+		case 6:
+			level = logging::Logger::ERROR_;
+			break;
+		case 7:
+		case 'h':
+			usage(logStream, argc, argv);
+			break;
+		case 8:
+			version(logStream, argc, argv);
+			break;
+		case '?':
+			exit(-1);
+			break;
+		default:
+		{
+			std::string arg(argv[index]);
+			if(index+1<argc && util::startsWith(arg, RESOLVE_PREFIX)){
+				++index;
+				this->resolveOpts.push_back(std::pair<std::string, std::string>(arg.substr(RESOLVE_PREFIX.size()), argv[index]));
+			}
+			break;
+		}
 		}
 	}
+
+	this->log = new logging::Logger(logStream, level);
+	this->bridge = new python::PyBridge(*this->log);
+
+	if(optind >= argc){
+		throw logging::Exception("You need to set video id!");
+	}else{
+		firstVideoId = argv[optind];
+		log->i(TAG, "First Video ID: %s", firstVideoId.c_str());
+	}
+
 }
 
 Saccubus::~Saccubus() {
+	if(currentContext){
+		delete currentContext;
+	}
 	delete bridge;
+	delete log;
 }
 
 void Saccubus::init(Adapter* const adapter)
 {
 	this->adapter = adapter;
+
+	//最初は自分でイベントを起こす（それ以降はNekomataで起こす）
+	this->onVideoChanged(firstVideoId);
 }
 
 void Saccubus::measure(const int w, const int h, int& measuredWidth, int& measuredHeight)
@@ -54,6 +150,23 @@ void Saccubus::measure(const int w, const int h, int& measuredWidth, int& measur
 void Saccubus::draw(float vpos, draw::Canvas* canvas, draw::Sprite* videoSprite)
 {
 
+}
+
+void Saccubus::onVideoChanged(const std::string& videoId)
+{
+	std::vector<std::pair<std::string, std::string> > arg(this->resolveOpts.begin(), this->resolveOpts.end());
+	arg.push_back(std::pair<std::string, std::string>("video-id", videoId));
+	meta::VideoContext* ctx = bridge->resolveResource(videoId, arg);
+	if(this->currentContext){
+		this->log->i("Context deleted: %s", this->currentContext->metaInfo()->title());
+		delete this->currentContext;
+	}
+	this->currentContext = ctx;
+	this->log->i("Context entered: %s", this->currentContext->metaInfo()->title());
+	if(adapter)
+	{
+		adapter->onVideoChanged(videoId, ctx->videofile());
+	}
 }
 
 }
--- src/saccubus/Saccubus.h
+++ src/saccubus/Saccubus.h
@@ -11,6 +11,7 @@
 #include <string>
 #include <map>
 #include <vector>
+#include <iostream>
 #include "classdefs.h"
 
 namespace saccubus {
@@ -18,9 +19,16 @@ namespace saccubus {
 class Adapter
 {
 public:
+	/**
+	 * 最初や、ニワン語でjumpが起こった時に呼ばれる。
+	 * このコールの次のdraw()には、ちゃんと指定された動画の内容を渡すこと。
+	 */
 	void onVideoChanged(const std::string& videoId, const std::string& filepath);
+	/**
+	 * ニワン語でseekが起こった時に呼ばれる。
+	 * このコールの次のdraw()には、ちゃんと指定された動画の内容を渡すこと。
+	 */
 	void onSeek(const std::string& videoId, float vpos);
-	void onFinished();
 };
 
 class Saccubus {
@@ -29,16 +37,19 @@ private:
 private:
 	const std::string progPath;
 	std::vector<std::pair<std::string, std::string> > resolveOpts;
-	std::string nowVideoId;
+	std::string firstVideoId;
+	meta::VideoContext* currentContext;
 private:
-	python::PyBridge* const bridge;
-public:
-	Saccubus(logging::Logger& log, int argc, char** argv);
+	python::PyBridge* bridge;
+	logging::Logger* log;
+public: //
+	Saccubus(std::ostream& logStream, int argc, char** argv);
 	virtual ~Saccubus();
-
+public: //公開メソッド
 	void init(Adapter* const adapter);
 	void measure(const int w, const int h, int& measuredWidth, int& measuredHeight);
 	void draw(float vpos, draw::Canvas* canvas, draw::Sprite* videoSprite);
+public: // 内部から呼ばれます
 	void onVideoChanged(const std::string& videoId);
 };
 
