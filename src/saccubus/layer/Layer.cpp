a0427711cadd320d8c0456e42d1571055d83e6b7
psi <ledyba@users.sourceforge.jp>
2012-04-13 15:32:43 +0900

    ロジカルな描画名前空間 layerの導入

---
+++ src/saccubus/layer/Layer.cpp
@@ -0,0 +1,22 @@
+/*
+ * Layer.cpp
+ *
+ *  Created on: 2012/03/25
+ *      Author: psi
+ */
+
+#include "Layer.h"
+
+namespace saccubus {
+namespace layer {
+
+Layer::Layer() {
+	// do nothing
+
+}
+
+Layer::~Layer() {
+	// do nothing
+}
+
+}}
---
+++ src/saccubus/layer/Layer.h
@@ -0,0 +1,31 @@
+/*
+ * Layer.h
+ *
+ *  Created on: 2012/03/25
+ *      Author: psi
+ */
+
+#ifndef LAYER_H_
+#define LAYER_H_
+
+#include "../draw/Canvas.h"
+
+namespace saccubus {
+namespace layer {
+
+/**
+ * コメントや図形の画面上の配置を計算したり、ニワン語との接続を行う、
+ * いわゆるビジネスロジックを配置するクラス。
+ * ！！　設計方針　！！
+ * ！！　具体的な描画ライブラリに依存するコードは書いてはいけない　！！
+ * ！！　それらはdraw名前空間以下のクラスに投げましょう　！！
+ */
+class Layer {
+public:
+	Layer();
+	virtual ~Layer();
+	virtual void draw(float vpos, draw::Canvas* canvas) = 0;
+};
+
+}}
+#endif /* LAYER_H_ */
