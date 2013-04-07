f70c54b95c2fc62110adcff09150d63d5de836be
psi <ledyba@users.sourceforge.jp>
2013-04-07 10:42:38 +0900

    パーサを作るシェルスクリプトを一本化

--- makeParser.sh
+++ makeParser.sh
@@ -2,26 +2,38 @@
 DIRNAME=$(cd $(dirname $0);pwd)
 
 ANTLR=${DIRNAME}/external/antlr/antlr-3.4-complete.jar
-ANTLR_SRC=${DIRNAME}/src/nekomata/parser
-ANTLR_OUT=${ANTLR_SRC}
+PARSER_SRC=${DIRNAME}/src/nekomata/parser
+PARSER_OUT=${PARSER_SRC}
+TRANS_SRC=${DIRNAME}/src/nekomata/trans
+TRANS_OUT=${TRANS_SRC}
 
 if [ "$MSYSTEM" = "MINGW32" ] ; then
 	CURDIR=${DIRNAME} \
-	ANTLR_SRC=${ANTLR_SRC} \
-	ANTLR_OUT=${ANTLR_OUT} \
+	PARSER_SRC=${PARSER_SRC} \
+	PARSER_OUT=${PARSER_OUT} \
+	TRANS_SRC=${TRANS_SRC} \
+	TRANS_OUT=${TRANS_OUT} \
 	ANTLR=${ANTLR} \
 	cmd << EOF
 set CLASSPATH=%CURDIR%\external\antlr\antlr-3.4-complete.jar;%CLASSPATH%
-java -Dfile.encoding=UTF-8 -Duser.language=en -Duser.region=US org.antlr.Tool -fo %ANTLR_OUT% -make %ANTLR_SRC%\niwango.g
+java -Dfile.encoding=UTF-8 -Duser.language=en -Duser.region=US org.antlr.Tool -fo %PARSER_OUT% -make %PARSER_SRC%\niwango.g
+java -Dfile.encoding=UTF-8 -Duser.language=en -Duser.region=US org.antlr.Tool -fo %TRANS_OUT% -make %TRANS_SRC%\NicosMessage.g
 EOF
 else
 	CLASSPATH=${ANTLR}:$CLASSPATH \
-	java org.antlr.Tool -fo ${ANTLR_OUT} -make \
-		${ANTLR_SRC}/niwango.g
+	java org.antlr.Tool -fo ${PARSER_OUT} -make \
+		${PARSER_SRC}/niwango.g
+	CLASSPATH=${ANTLR}:$CLASSPATH \
+	java org.antlr.Tool -fo ${TRANS_OUT} -make \
+		${TRANS_SRC}/NicosMessage.g
 fi
 
-find ${ANTLR_OUT} -type f -name *.c | while read f; do
-  mv $f ${ANTLR_OUT}/$(basename $f .c).cpp
+find ${PARSER_OUT} -type f -name *.c | while read f; do
+  mv $f ${PARSER_OUT}/$(basename $f .c).cpp
+done
+
+find ${TRANS_OUT} -type f -name *.c | while read f; do
+  mv $f ${TRANS_OUT}/$(basename $f .c).cpp
 done
 
 
