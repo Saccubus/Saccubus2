887ec2c71efce1e0e3930498b64cc7eb9952d951
psi <ledyba@users.sourceforge.jp>
2013-11-26 18:13:16 +0900

    implement get

--- Saccubus/Network.fs
+++ Saccubus/Network.fs
@@ -1,7 +1,37 @@
 ﻿namespace Saccubus.Network
 
+open System
+open System.IO;
+open System.Net;
+open System.Web;
+open System.Text;
+open System.Collections;
+open Functional
+
 module Http =
-    let e = 1
+    let getBytes (url:string) : Either<Exception, byte[]> = 
+        let cl = new System.Net.WebClient();
+        Right (cl.DownloadData(url))
+
+    let getString (url:string) (enc:string) : Either<Exception, string> = 
+        let enc = System.Text.Encoding.GetEncoding(enc)
+        let cl = new System.Net.WebClient();
+        let st = cl.OpenRead(url)
+        let sr = new System.IO.StreamReader(st, enc)
+        try
+            Right ( sr.ReadToEnd() )
+        finally
+            sr.Close()
+            st.Close()
+    
+    let getAsyncString (url:string) (enc:string) (cont:Either<Exception, string> -> 'a) =
+        cont (getString url enc);
+
+    let getAsyncBytes (url:string) (enc:string) (cont:Either<Exception, byte[]> -> 'a) =
+        cont (getBytes url);
+
+    let postBytes (url:string) (hash:Hashtable) = 
+        null
 
 module Html =
     let e = 1
